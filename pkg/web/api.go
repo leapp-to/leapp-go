99cc69667b4479d495144b7c5dbdf43c9652c186
mfranczy <mfranczy@redhat.com>
2017-09-25 13:16:56 +0200

    instead of passing decoder, pass pointer to request

--- pkg/web/api.go
+++ pkg/web/api.go
@@ -3,29 +3,30 @@ package web
 import (
 	"encoding/json"
 	"errors"
+	"net/http"
 )
 
 type MigrateParams struct {
-	SourceHost       string          `json:"source_host,omitempty"`
-	TargetHost       string          `json:"target_target,omitempty"`
-	ContainerName    string          `json:"container_name,omitempty"`
-	SourceUser       string          `json:"source_user,omitempty"`
-	TargetUser       string          `json:"target_user,omitempty"`
-	ExcludePaths     []string        `json:"excluded_paths,omitempty"`
-	TcpPorts         map[int16]int16 `json:"tcp_ports,omitempty"`
-	ExcludedTcpPorts []int16         `json:"excluded_tcp_ports,omitempty"`
-	ForceCreate      bool            `json:"force_create,omitempty"`
-	DisableStart     bool            `json:"disable_start,omitempty"`
-	Debug            bool            `json:"debug,omitempty"`
+	SourceHost       string            `json:"source_host,omitempty"`
+	TargetHost       string            `json:"target_target,omitempty"`
+	ContainerName    string            `json:"container_name,omitempty"`
+	SourceUser       string            `json:"source_user,omitempty"`
+	TargetUser       string            `json:"target_user,omitempty"`
+	ExcludePaths     []string          `json:"excluded_paths,omitempty"`
+	TcpPorts         map[uint16]uint16 `json:"tcp_ports,omitempty"`
+	ExcludedTcpPorts []uint16          `json:"excluded_tcp_ports,omitempty"`
+	ForceCreate      bool              `json:"force_create,omitempty"`
+	DisableStart     bool              `json:"disable_start,omitempty"`
+	Debug            bool              `json:"debug,omitempty"`
 }
 
-func MigrateMachine(decoder *json.Decoder) (map[string]interface{}, error) {
+func MigrateMachine(request *http.Request) (interface{}, error) {
 	var (
 		migrateParams MigrateParams
 		data          interface{}
 	)
 
-	if err := decoder.Decode(&migrateParams); err != nil {
+	if err := json.NewDecoder(request.Body).Decode(&migrateParams); err != nil {
 		return nil, err
 	}
 
