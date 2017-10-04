7cc8f1cfef6d1e1cf470cf2475ce03511d295e44
Fabio Bertinatto <fbertina@redhat.com>
2017-10-04 14:53:33 +0200

    Fix issues reported by linter

--- pkg/api/destroy_container.go
+++ pkg/api/destroy_container.go
@@ -2,41 +2,42 @@ package api
 
 import (
 	"encoding/json"
-	"github.com/leapp-to/leapp-go/pkg/executor"
 	"log"
 	"net/http"
+
+	"github.com/leapp-to/leapp-go/pkg/executor"
 )
 
-type DestroyContainerParams struct {
+type destroyContainerParams struct {
 	ContainerName string `json:"container_name"`
 	TargetHost    string `json:"target_host"`
 	TargetUser    string `json:"target_user"`
 }
 
-func DestroyContainer(request *http.Request) (interface{}, error) {
-	var destroyParams DestroyContainerParams
+func destroyContainerHandler(request *http.Request) (interface{}, error) {
+	var params destroyContainerParams
 
-	if err := json.NewDecoder(request.Body).Decode(&destroyParams); err != nil {
+	if err := json.NewDecoder(request.Body).Decode(&params); err != nil {
 		return nil, err
 	}
 
-	data := map[string]interface{}{
-		"container_name":   ObjValue{destroyParams.ContainerName},
-		"target_host":      ObjValue{destroyParams.TargetHost},
-		"target_user_name": ObjValue{destroyParams.TargetUser}}
+	d := map[string]interface{}{
+		"container_name":   ObjValue{params.ContainerName},
+		"target_host":      ObjValue{params.TargetHost},
+		"target_user_name": ObjValue{params.TargetUser},
+	}
 
-	json_data, err := json.Marshal(data)
+	actorInput, err := json.Marshal(d)
 	if err != nil {
 		return "", err
 	}
 
-	exec := executor.New("destroy-container", string(json_data))
-	result := exec.Execute()
+	c := executor.New("destroy-container", string(actorInput))
+	r := c.Execute()
 
-	log.Println(result.Stderr)
+	log.Println(r.Stderr)
 
 	var out interface{}
-	err = json.Unmarshal([]byte(result.Stdout), &out)
-
+	err = json.Unmarshal([]byte(r.Stdout), &out)
 	return out, err
 }
--- pkg/api/port_map.go
+++ pkg/api/port_map.go
@@ -8,39 +8,40 @@ import (
 	"github.com/leapp-to/leapp-go/pkg/executor"
 )
 
-type PortMapParams struct {
+type portMapParams struct {
 	SourceHost       string              `json:"source_host"`
 	TargetHost       string              `json:"target_host"`
-	TcpPorts         TCPPortsUserMapping `json:"tcp_ports"`
-	ExcludedTcpPorts ExcludedTCPPorts    `json:"excluded_tcp_ports"`
+	TCPPorts         TCPPortsUserMapping `json:"tcp_ports"`
+	ExcludedTCPPorts ExcludedTCPPorts    `json:"excluded_tcp_ports"`
 	DefaultPortMap   bool                `json:"default_port_map"`
 }
 
-func PortMap(request *http.Request) (interface{}, error) {
-	var portMapParams PortMapParams
+func portMapHandler(request *http.Request) (interface{}, error) {
+	var params portMapParams
 
-	if err := json.NewDecoder(request.Body).Decode(&portMapParams); err != nil {
+	if err := json.NewDecoder(request.Body).Decode(&params); err != nil {
 		return nil, err
 	}
 
-	data := map[string]interface{}{
-		"source_host":            ObjValue{portMapParams.SourceHost},
-		"target_host":            ObjValue{portMapParams.TargetHost},
-		"use_default_port_map":   ObjValue{portMapParams.DefaultPortMap},
-		"tcp_ports_user_mapping": portMapParams.TcpPorts,
-		"excluded_tcp_ports":     portMapParams.ExcludedTcpPorts}
+	d := map[string]interface{}{
+		"source_host":            ObjValue{params.SourceHost},
+		"target_host":            ObjValue{params.TargetHost},
+		"use_default_port_map":   ObjValue{params.DefaultPortMap},
+		"tcp_ports_user_mapping": params.TCPPorts,
+		"excluded_tcp_ports":     params.ExcludedTCPPorts,
+	}
 
-	json_data, err := json.Marshal(data)
+	actorInput, err := json.Marshal(d)
 	if err != nil {
 		return "", err
 	}
 
-	exec := executor.New("port-mapping", string(json_data))
-	result := exec.Execute()
+	c := executor.New("port-mapping", string(actorInput))
+	r := c.Execute()
 
-	log.Println(result.Stderr)
+	log.Println(r.Stderr)
 
 	var out interface{}
-	json.Unmarshal([]byte(result.Stdout), &out)
+	json.Unmarshal([]byte(r.Stdout), &out)
 	return out, err
 }
