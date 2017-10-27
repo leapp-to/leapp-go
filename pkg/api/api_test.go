f7fea0fc656a4448fa86380a9a951659d1d62ecc
Fabio Bertinatto <fbertina@redhat.com>
2017-10-27 13:55:09 +0200

    Add test for respHandler

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -1,132 +1,98 @@
 package api
 
 import (
-	"context"
-	"encoding/json"
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	"github.com/leapp-to/leapp-go/pkg/executor"
+	"github.com/pkg/errors"
 )
 
-type TestResponseWriter struct {
-	Response string
-	header   http.Header
+type params struct {
+	data   interface{}
+	status int
+	err    error
 }
 
-func (wr *TestResponseWriter) Header() http.Header {
-	if nil == wr.header {
-		wr.header = http.Header{}
-	}
-	return wr.header
-}
-
-func (wr *TestResponseWriter) Write(str []byte) (int, error) {
-	wr.Response += string(str)
-	return 0, nil
-}
-
-func (wr *TestResponseWriter) WriteHeader(code int) {
-	return
-}
-
-func generateTestRequest() *http.Request {
-	request := http.Request{}
-	ctx := context.WithValue(context.Background(), CKey("Verbose"), false)
-	return request.WithContext(ctx)
-}
-
-func generateTestWriter() TestResponseWriter {
-	return TestResponseWriter{
-		Response: "",
-	}
-}
-
-func mockAPIHandlerSuccess(request *http.Request) (*executor.Command, error) {
-	c := executor.NewProcess("echo", "{\"out\": 0}")
-	return c, nil
-}
-
-func mockAPIHandlerErrorEmptyOut(request *http.Request) (*executor.Command, error) {
-	c := executor.NewProcess("true")
-	return c, nil
-}
-
-func mockAPIHandlerErrorCorruptedOut(request *http.Request) (*executor.Command, error) {
-	c := executor.NewProcess("echo", "{\"out: 0}")
-	return c, nil
-}
-
-func mockAPIHandlerErrorCmdFail(request *http.Request) (*executor.Command, error) {
-	c := executor.NewProcess("ls", "/non-existing-file")
-	return c, nil
-}
-
-func TestGenericResponseHandler(t *testing.T) {
-	request := generateTestRequest()
-	writer := generateTestWriter()
-
-	// Scenarios:
-	// No output
-	// Corrupted output
-	// Cmd fail
-	// Success
-	// Check content type for all?
-	//compare output with expected output
-	var scenarios = []struct {
-		fn      func(request *http.Request) (*executor.Command, error)
-		success bool
-		code    int
+func TestResponseHandler(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    params
+		expected params
 	}{
 		{
-			fn:      mockAPIHandlerSuccess,
-			success: true,
-			code:    0,
+			name: "internal error",
+			input: params{
+				data:   nil,
+				status: 0,
+				err:    errors.New("this is a random error"),
+			},
+			expected: params{
+				data:   "Internal error",
+				status: http.StatusInternalServerError,
+			},
 		},
 		{
-			fn:      mockAPIHandlerErrorEmptyOut,
-			success: false,
-			code:    3,
+			name: "bad input error",
+			input: params{
+				data:   nil,
+				status: http.StatusBadRequest,
+				err:    newAPIError(nil, errBadInput, "bad input msg"),
+			},
+			expected: params{
+				data:   fmt.Sprintf(`{"errors":[{"code":%d,"message":%q}]}`, errBadInput, "bad input msg"),
+				status: http.StatusBadRequest,
+			},
 		},
 		{
-			fn:      mockAPIHandlerErrorCorruptedOut,
-			success: false,
-			code:    3,
-		},
-		{
-			fn:      mockAPIHandlerErrorCmdFail,
-			success: false,
-			code:    2,
+			name: "correct data",
+			input: params{
+				data:   map[string]string{"id": "123", "name": "test"},
+				status: http.StatusOK,
+				err:    nil,
+			},
+			expected: params{
+				data:   fmt.Sprintf(`{"data":{"id":"123","name":"test"}}`),
+				status: http.StatusOK,
+			},
 		},
 	}
 
-	for i, s := range scenarios {
-		response := &writer.Response
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
 
-		m := genericResponseHandler(s.fn)
-		m(&writer, request)
-
-		var tmp interface{}
-		if err := json.Unmarshal([]byte(*response), &tmp); nil != err {
-			t.Errorf("Unexpected data received during scenario execution")
-		}
-		result := tmp.(map[string]interface{})
-
-		if _, ok := result["data"]; ok && s.success {
-		} else if errListInterface, ok := result["errors"]; ok && !s.success {
-			errorsList := errListInterface.([]interface{})
-			// Only one code is expected although the errors is containing a list
-			for _, eInterface := range errorsList {
-				e := eInterface.(map[string]interface{})
-				ecode := int(e["code"].(float64))
-				if ecode != s.code {
-					t.Errorf("Expected and received error code does not match. Expect %d got %d", s.code, ecode)
-				}
+			fn := func(rw http.ResponseWriter, req *http.Request) (interface{}, int, error) {
+				return tc.input.data, tc.input.status, tc.input.err
 			}
-		} else {
-			t.Errorf("Unexpected result in scenario %d", i)
-		}
+			hn := respHandler(fn)
 
-		*response = ""
+			req, err := http.NewRequest("GET", "sample_url", nil)
+			if err != nil {
+				t.Fatalf("could not create request: %v", err)
+			}
+			rec := httptest.NewRecorder()
+			hn(rec, req)
+
+			res := rec.Result()
+
+			b, err := ioutil.ReadAll(res.Body)
+			defer res.Body.Close()
+			if err != nil {
+				t.Fatalf("could not read response body: %v", err)
+			}
+
+			if data := string(bytes.TrimSpace(b)); data != tc.expected.data {
+				t.Errorf("expected data %v; got %v", tc.expected.data, data)
+			}
+
+			if res.StatusCode != tc.expected.status {
+				t.Errorf("expected status %v; got %v", tc.expected.status, res.StatusCode)
+			}
+
+		})
 	}
+
 }
